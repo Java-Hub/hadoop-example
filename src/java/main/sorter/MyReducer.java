59fb3768b58302afd4a6df8bc819a3b05df650b5
CK <CK@CK-PC>
2020-10-21 20:22:59 +0800

    1

--- src/java/main/sorter/MyReducer.java
+++ src/java/main/sorter/MyReducer.java
@@ -3,7 +3,6 @@ package sorter;
 import java.io.IOException;
 
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class MyReducer extends Reducer<MyKey, NullWritable, MyKey, NullWritable> {
